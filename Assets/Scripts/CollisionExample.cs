55db8a9a1268d76dc9112f8b5cec60dfe06a6574
suibiweng <suibi.weng@gmail.com>
2023-03-02 13:14:07 -0700

    TouchComand

--- Assets/Scripts/CollisionExample.cs
+++ Assets/Scripts/CollisionExample.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CollisionExample : MonoBehaviour
 {
+    public CmdCallSpot spot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spot = GameObject.FindObjectOfType<CmdCallSpot>().GetComponent<CmdCallSpot>();
+        spot.StartBat();
+        spot.Task(1);
     }
 
     // Update is called once per frame
@@ -16,10 +19,25 @@ public class CollisionExample : MonoBehaviour
         
     }
 
+    bool TouchOne=false;
+
     void OnCollisionEnter(Collision other) {
-
-
         print("Touch");
+
+        if(!TouchOne)
+    {
+        TouchOne=true;
+
+        spot.Task(1);
+    }
+       
+        
+        
+    }
+
+     void OnCollisionExit(Collision other) {
+
+        TouchOne=false;
         
     }
 
